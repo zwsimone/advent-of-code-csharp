32835482c66bb2957b451f2530319267b933daae
Simone Chan <zwsimone@gmail.com>
2023-12-01 10:10:25 +0200

    Add utilities for parsing file input

---
+++ AdventOfCode/Utilities/FileUtilities.cs
@@ -0,0 +1,40 @@
+﻿namespace Utilities;
+
+public static class FileUtilities
+{
+    public static List<string> GetListFromFile(string path)
+    {
+        List<string> list = new();
+        if (File.Exists(path))
+        {
+            list = File.ReadAllLines(path).ToList();
+        }
+        else
+        {
+            Console.WriteLine($"File not found: {path}");
+        }
+        
+        return list;
+    }
+    
+    public static string GetProjectSolutionPath()
+    {
+        string? parentDirectoryName = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+        if (parentDirectoryName != null)
+        {
+            DirectoryInfo programDirectoryInfo = new DirectoryInfo(parentDirectoryName);
+
+            DirectoryInfo? parentDirectory = programDirectoryInfo.Parent; // Get the parent directory (directory "B" in your example)
+
+            return parentDirectory?.FullName ?? string.Empty;
+        }
+
+        return string.Empty;
+    }
+    
+    public static string GetFolderNameFromPath(string path)
+    {
+        string[] pathParts = path.Split(Path.DirectorySeparatorChar);
+        return pathParts[^1];
+    }
+}

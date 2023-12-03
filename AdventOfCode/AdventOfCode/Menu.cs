883646202c44bc29acff4415071788235ca4d291
Simone Chan <zwsimone@gmail.com>
2023-12-03 07:35:18 +0200

    Add code runner

--- AdventOfCode/AdventOfCode/Menu.cs
+++ AdventOfCode/AdventOfCode/Menu.cs
@@ -1,6 +1,75 @@
-﻿namespace AdventOfCode;
-
-public class Menu
+﻿namespace AdventOfCode
 {
-    
+    public class Menu
+    {
+        private int _selectedIndex;
+        private readonly string[] _options;
+        private readonly string _prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            _prompt = prompt;
+            _options = options;
+            _selectedIndex = 0;
+        }
+
+        private void DisplayOptions()
+        {
+            Console.WriteLine(_prompt);
+            for (int i = 0; i < _options.Length; i++)
+            {
+                string currentOption = _options[i];
+                string prefix;
+                if (i == _selectedIndex)
+                {
+                    prefix = "*";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine($"{prefix} << {currentOption} >>");
+            }
+
+            Console.ResetColor();
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // Update SelectedIndex based on arrow keys.
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    _selectedIndex--;
+                    if (_selectedIndex == -1)
+                    {
+                        _selectedIndex = _options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    _selectedIndex++;
+                    if (_selectedIndex == _options.Length)
+                    {
+                        _selectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+
+            return _selectedIndex;
+        }
+    }
 }
+
---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Reflection;
+using Utilities;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        private static readonly string SolutionPath = FileUtilities.GetProjectSolutionPath();
+        private static readonly string[] AocFolderPaths = Directory.GetDirectories(SolutionPath, "AOC*");
+
+        private static void RunMainMenu()
+        {
+            string prompt = @"
+ █████╗ ██████╗ ██╗   ██╗███████╗███╗   ██╗████████╗     ██████╗ ███████╗     ██████╗ ██████╗ ██████╗ ███████╗    ██████╗ ██╗   ██╗███╗   ██╗███╗   ██╗███████╗██████╗ 
+██╔══██╗██╔══██╗██║   ██║██╔════╝████╗  ██║╚══██╔══╝    ██╔═══██╗██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝    ██╔══██╗██║   ██║████╗  ██║████╗  ██║██╔════╝██╔══██╗
+███████║██║  ██║██║   ██║█████╗  ██╔██╗ ██║   ██║       ██║   ██║█████╗      ██║     ██║   ██║██║  ██║█████╗      ██████╔╝██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
+██╔══██║██║  ██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║   ██║       ██║   ██║██╔══╝      ██║     ██║   ██║██║  ██║██╔══╝      ██╔══██╗██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝ ╚████╔╝ ███████╗██║ ╚████║   ██║       ╚██████╔╝██║         ╚██████╗╚██████╔╝██████╔╝███████╗    ██║  ██║╚██████╔╝██║ ╚████║██║ ╚████║███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝        ╚═════╝ ╚═╝          ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
+
+Welcome to the Advent of Code Runner.
+Please select one of the following options.
+(Use arrow keys to navigate, press enter to select)";
+            string[] options = { "Latest", "Custom", "Exit" };
+            Menu mainMenu = new Menu(prompt, options);
+            int selectedIndex = mainMenu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    string yearFolderName = FileUtilities.GetFolderNameFromPath(AocFolderPaths[^1]);
+                    string dayFolderName = FileUtilities.GetFolderNameFromPath(Directory.GetDirectories(AocFolderPaths[^1], "Day*")[^1]);
+                    RunDay(yearFolderName, dayFolderName);
+                    break;
+                case 1:
+                    RunCustomMenu();
+                    break;
+                case 2:
+                    ExitGame();
+                    break;
+            }
+        }
+
+        private static void RunCustomMenu()
+        {
+            string prompt = @"
+Please select the year.
+(Use arrow keys to navigate, press enter to select)";
+            List<string> options = new List<string>();
+            foreach (string s in AocFolderPaths.ToList())
+            {
+                options.Add(FileUtilities.GetFolderNameFromPath(s));
+            }
+            options.Add("Back");
+            Menu mainMenu = new Menu(prompt, options.ToArray());
+            int selectedIndex = mainMenu.Run();
+            
+            if (selectedIndex == options.Count - 1)
+            {
+                RunMainMenu();
+            }
+            else
+            {
+                string yearFolderName = options[selectedIndex];
+                RunDayMenu(yearFolderName, selectedIndex);
+            }
+        }
+
+        private static void RunDayMenu(string yearFolderName, int yearIndex)
+        {
+            string prompt = @"
+Please select the day.
+(Use arrow keys to navigate, press enter to select)";
+            List<string> options = new List<string>();
+            string[] dayFolders = Directory.GetDirectories(AocFolderPaths[yearIndex], "Day*");
+            foreach (string s in dayFolders.ToList())
+            {
+                options.Add(FileUtilities.GetFolderNameFromPath(s));
+            }
+
+            options.Add("Back");
+            Menu dayMenu = new Menu(prompt, options.ToArray());
+            int selectedIndex = dayMenu.Run();
+
+            if (selectedIndex == options.Count - 1)
+            {
+                RunMainMenu();
+            }
+            else
+            {
+                string dayFolderName = options[selectedIndex];
+                RunDay(yearFolderName, dayFolderName);
+            }
+        }
+
+        private static void RunDay(string yearFolderName, string dayFolderName)
+        {
+            Console.WriteLine("Running {0}, {1}...", yearFolderName, dayFolderName);
+            Type? type = Type.GetType($"{yearFolderName}.{dayFolderName}.{dayFolderName}, {yearFolderName}");
+            if (type != null)
+            {
+                object instance = Activator.CreateInstance(type);
+                MethodInfo methodInfo = type.GetMethod("Run");
+                if (methodInfo != null)
+                {
+                    methodInfo.Invoke(instance, null);
+                }
+                else
+                {
+                    Console.WriteLine("Method 'Run' not found: {0}.{1}", yearFolderName, dayFolderName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Class '{0}' not found in namespace '{1}.{0}'", dayFolderName, yearFolderName);
+            }
+        }
+
+        private static void ExitGame()
+        {
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            RunMainMenu();
+        }
+    }
+}
